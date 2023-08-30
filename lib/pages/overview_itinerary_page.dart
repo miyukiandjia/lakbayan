f5edaaed20e895413f6f00dbb07428892b6a03c9
jcpanggoyyy <jcpanggoyyy23@gmail.com>
2023-08-30 17:16:53 +0800

    try

---
+++ lib/pages/overview_itinerary_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OverviewItinerary extends StatelessWidget {
+  const OverviewItinerary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Implement your Create Itinerary screen UI here
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Itinerary'),
+      ),
+      body: const Center(
+        child: Text('This is the Create Itinerary screen'),
+      ),
+    );
+  }
+}
